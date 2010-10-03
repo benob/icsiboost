178f9d4107cfa421c551e4567629ddfcd4edce95
Benoit Favre <benoit.favre@lif.univ-mrs.fr>
2010-10-03 19:24:21 +0000

    download files in test_uci.sh only if necessary

--- icsiboost/src/test_uci.sh
+++ icsiboost/src/test_uci.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.names
-wget http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data
-wget http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test
+[ -f adult.names ] || wget http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.names -O adult.names
+[ -f adult.data ] || wget http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data -O adult.data
+[ -f adult.test ] || wget http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test -O adult.test
 ./icsiboost -n 100 -S adult
